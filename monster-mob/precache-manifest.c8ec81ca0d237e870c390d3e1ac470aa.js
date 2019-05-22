8c503c6f0397a36a50517deaa74b7849ad363b70
scotnewbury <scot@scotnewbury.com>
2019-05-22 15:53:10 -0400

    rebuilt monster-mob, adding it to site

---
+++ monster-mob/precache-manifest.c8ec81ca0d237e870c390d3e1ac470aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cad847ef29d166ee4d4",
+    "url": "/static/js/main.56b9cfef.chunk.js"
+  },
+  {
+    "revision": "0fb20dc6ec981a722e57",
+    "url": "/static/js/2.ec9ba955.chunk.js"
+  },
+  {
+    "revision": "1cad847ef29d166ee4d4",
+    "url": "/static/css/main.7ca9f39c.chunk.css"
+  },
+  {
+    "revision": "0fb20dc6ec981a722e57",
+    "url": "/static/css/2.fb3f8356.chunk.css"
+  },
+  {
+    "revision": "e7758aa5160d7e54b542a2685934fb36",
+    "url": "/index.html"
+  }
+];
