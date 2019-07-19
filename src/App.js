227a9694abf1f325d0137827ddea00dee79e7acf
scotnewbury <scot@scotnewbury.com>
2019-07-19 18:00:58 -0400

    Clean up of app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Projects from './components/pages/Projects';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
@@ -11,25 +11,33 @@ export class App extends Component {
       <Router>
         <div>
           <Header />
-          <Route exact path = "/" render = {props => (
-            <React.Fragment>
-              <div className="masthead text-center">
-                <h1>Scot Newbury</h1>
-                <h3>Web Developer and Technology Enthusiast</h3>
-                <a href="https://github.com/scotnewbury"><i className="fab fa-github"></i></a>
-                <a href="http://www.linkedin.com/in/scotnewbury"><i className="fab fa-linkedin"></i></a>
-                <a href="mailto:scot@scotnewbury.com"><i className="fas fa-envelope"></i></a>
-              </div>
-          </React.Fragment>
-          )} />
-          <Route path = "/projects" component = {Projects} />
+          <Route
+            exact
+            path='/'
+            render={props => (
+              <React.Fragment>
+                <div className='masthead text-center'>
+                  <h1>Scot Newbury</h1>
+                  <h3>Web Developer and Technology Enthusiast</h3>
+                  <a href='https://github.com/scotnewbury'>
+                    <i className='fab fa-github' />
+                  </a>
+                  <a href='http://www.linkedin.com/in/scotnewbury'>
+                    <i className='fab fa-linkedin' />
+                  </a>
+                  <a href='mailto:scot@scotnewbury.com'>
+                    <i className='fas fa-envelope' />
+                  </a>
+                </div>
+              </React.Fragment>
+            )}
+          />
+          <Route path='/projects' component={Projects} />
         </div>
         <Footer />
       </Router>
-      
-    )
+    );
   }
 }
 
-
-export default App
+export default App;
